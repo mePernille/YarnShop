d4917548f00517f53c6ead3dbedc213d356a9650
mePernille <89570696+mePernille@users.noreply.github.com>
2023-09-27 21:38:27 +0200

    lagt til grid og table

--- Controllers/ItemController.cs
+++ Controllers/ItemController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Models; // this will identifie the Item Class
+using WebApplication1.ViewModels;
+
 
 namespace WebApplication1.Controllers;
 
@@ -11,23 +13,51 @@ public class ItemController : Controller
 {
     public IActionResult Table()
     {
+        var items = GetItems();
+        var itemListViewModel = new ItemListViewModel(items, "Table");
+        return View(itemListViewModel);
+    }
+
+    public IActionResult Grid()
+    {
+        var items = GetItems();
+        var itemListViewModel = new ItemListViewModel(items, "Grid");
+        return View(itemListViewModel);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var items = GetItems();
+        var item = items.FirstOrDefault(item => item.ItemId == id);
+        if (item == null)
+            return NotFound();
+        return View(item);
+    }
+
+    public List<Item> GetItems()
+    {
+
         var items = new List<Item>();
-        var item1 = new Item();
-        item1.ItemId = 1;
-        item1.Name = "Finull";
-        item1.Price = 60;
+        var item1 = new Item
+        {
+            ItemId = 1,
+            Name = "Finull",
+            Price = 60,
+            Description = "Dejlig varmt og blødt uld garn",
+            ImageUrl = "/images/fancyGarn.jpg"
+        };
 
         var item2 = new Item // this way of adding items is prefered by Visual Studio
         {
             ItemId = 2,
             Name = "Alpakka ull",
-            Price = 86
+            Price = 86,
+            Description="Super flot og dyrt garn",
+            ImageUrl = "/images/masseGarn.jpg"
         };
 
         items.Add(item1);
         items.Add(item2);
-
-        ViewBag.CurrentViewName = "Garn ";
-        return View(items);
+        return items;
     }
 }
---
+++ ViewModels/ItemListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+
+namespace WebApplication1.ViewModels
+{
+    public class ItemListViewModel
+    {
+        public IEnumerable<Item> Items;
+        public string? CurrentViewName;
+
+        public ItemListViewModel(IEnumerable<Item> items, string? currentViewName) 
+        { 
+        
+            Items = items;
+            CurrentViewName = currentViewName;
+        }
+    }
+}
